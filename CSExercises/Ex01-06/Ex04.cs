8e2c9379eba63de3add48d5b4eff726c864838ff
kimmy-a0137828u <e00001344@u.nus.edu>
2015-08-17 12:23:11 +0800

    Completed

--- CSExercises/Ex01-06/Ex04.cs
+++ CSExercises/Ex01-06/Ex04.cs
@@ -10,8 +10,11 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            double Number = 36;
-            double SqrtNumber; 
+            double Number;
+            double SqrtNumber;
+            Console.WriteLine("Please enter a number:");
+            String c = Console.ReadLine();
+            Number = Convert.ToDouble(c); 
             SqrtNumber = Math.Sqrt(Number);
             Console.WriteLine("Square root of {0} is {1}.", Number, SqrtNumber);
             
